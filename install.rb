334185d5bdca2edd4945ec64fca327389a997f73
Luke Yeager <luke.yeager@gmail.com>
2014-07-30 16:58:06 -0500

    Created an install script
    Only works on linux atm

---
+++ install.rb
@@ -0,0 +1,36 @@
+module OS
+  def OS.windows?
+    (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+  end
+
+  def OS.mac?
+   (/darwin/ =~ RUBY_PLATFORM) != nil
+  end
+
+  def OS.unix?
+    !OS.windows?
+  end
+
+  def OS.linux?
+    OS.unix? and not OS.mac?
+  end
+end
+
+if OS.linux?
+	ruby_exe = `which ruby`.chomp
+	if !File.exists?(ruby_exe)
+		puts 'ERR: Ruby not found... but this is a ruby script... weird.'
+		return
+	end
+	script = File.join(__dir__, 'dynipo.rb')
+	if !File.exists?(script)
+		puts "ERR: Can't find the dynipo.rb script."
+		return
+	end
+	# Install
+	`crontab -l | { cat; echo "\n#Dynipo updates every 10 minutes\n*/10 * * * * ruby #{File.join(__dir__, 'dynipo.rb')}"; } | crontab -`
+elsif OS.windows?
+	puts 'Windows support coming soon...'
+else
+	puts 'Your platform is not supported.'
+end
